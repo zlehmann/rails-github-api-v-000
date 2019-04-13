826d5fb1cfcab7040c1073dee1d010ca883ac30e
Zachary Lehmann <zlehmann@gmail.com>
2019-04-13 23:57:32 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,9 @@ class SessionsController < ApplicationController
       req.body = { 'client_id': ENV['GITHUB_CLIENT_ID'], 'client_secret': ENV['GITHUB_CLIENT_SECRET'], 'code': params[:code] }
       req.headers['Accept'] = 'application/json'
     end
+
+    body = JSON.parse(response.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
   end
 end
