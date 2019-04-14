3b9772e7097747c35550832a558cbf25f58121f4
Zachary Lehmann <zlehmann@gmail.com>
2019-04-14 00:17:51 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,7 +2,7 @@ class RepositoriesController < ApplicationController
 
   def index
     resp = Faraday.get("https://api.github.com/user") do |req|
-      req.params['oauth_token'] = session[:token]
+      req.params['access_token'] = session[:token]
     end
     body = JSON.parse(resp.body)
     @username = body
