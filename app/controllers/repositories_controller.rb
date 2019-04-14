a7912d33004c152f1804e6626cbe3dcff30b343a
Zachary Lehmann <zlehmann@gmail.com>
2019-04-14 00:05:41 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,5 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
     resp = Faraday.get("https://api.github.com/user") do |req|
       req.params['oauth_token'] = session[:token]
